71e7add0fb23155e9133a3dedce32bb1249a475d
Gustavo <gustavotrevezani@hotmail.com>
2025-03-28 17:43:39 -0300

    finalizado

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 <h1>testing</h1>
+<p>Para rodar o programa use</p>
+<p>nodemon src/server.js localhost:3000</p>
--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
-const vazio = '';
+const express = require('express')
+const path = require('path'); 
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+  });
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'register.html'));
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
